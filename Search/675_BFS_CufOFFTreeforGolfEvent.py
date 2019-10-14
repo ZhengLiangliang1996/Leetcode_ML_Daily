e7e46d7e16883f87016dd49ae3608e2e60ea8b51
Zheng LiangLiang <zhengliangliang1996@gmail.com>
2019-10-14 20:19:52 +0200

    update 675

--- Search/675_BFS_CufOFFTreeforGolfEvent.py
+++ Search/675_BFS_CufOFFTreeforGolfEvent.py
@@ -5,43 +5,82 @@ class Solution(object):
         :rtype: int
         """
         # create queue and enqueue first point
-        queue = [(0, 0)]
+        tree = []
 
         cols = len(forest[0])
         rows = len(forest)
 
-        step = -1
-        zeroN = 0
+        #put height into 
         for i in range(rows):
             for j in range(cols):
-                if forest[i][j] == 0:
-                    zeroN = zeroN + 1
-        # if the first tree is 0
-        if forest[0][0] == 0:
-            return step
-        
-        while queue:
-            # pop from queue
-            x, y = queue.pop(0)
-            
-            for (a, b) in [(x+1, y), (x-1, y), (x, y-1), (x, y + 1)]:
-                
-                if 0<=a<rows and 0<=b<cols and forest[a][b] != 0 and forest[x][y] < forest[a][b]:
-                    # enqueue
-                    queue.append((a, b))
-            step = step + 1
+                if forest[i][j] > 1:
+                    tree.append((forest[i][j], i, j))
 
-        return step
-        # if step+1 == cols*rows - zeroN:
-        #     return step
-        # else:
-        #     return -1
+        def takeFirst(elem):
+            return elem[0]
+        # sort by height 
+        tree.sort(key=takeFirst)
+
+        sx = 0
+        sy = 0
+
+        totalStep = 0
+
+        # BFS
+        def BFS(forest, sx, sy, tx, ty):
+            seen = [[0 for i in range(cols)] for j in range(rows)]
+
+            q = []
+            q.append((sx, sy))
+            steps = 0
+
+            while q:
+                new_nodes = len(q)
+                while new_nodes > 0:
+                    cx, cy = q.pop(0)
+
+                    # found the shortest path
+                    if cx == tx and cy == ty:
+                        return steps
+                    
+                    for x, y in [(cx + 1, cy), (cx - 1, cy), (cx, cy + 1), (cx, cy - 1)]:
+                        # out of bounds 
+                        if(x < 0 or x == rows or y < 0 or y == cols or not forest[x][y] or seen[x][y]):
+                            continue
+                        
+                        # mark x y as visited
+                        seen[x][y] = 1
+                        q.append((x, y))
+            
+            return float('inf')
+
+        # Move from current position to next tree to cut
+        for i in range(len(tree)):
+            tx = tree[i][1]
+            ty = tree[i][2]
+
+            steps = BFS(forest, sx, sy, tx, ty)
+
+            if steps == float('inf'):
+                return -1
+
+            totalStep = totalStep + steps
+
+            sx = ty
+            sy = ty
+        
+        return totalStep
 
 def main():
     S = Solution()
 
-    a = [[54581641,64080174,24346381,69107959],[86374198,61363882,68783324,79706116],[668150,92178815,89819108,94701471],[83920491,22724204,46281641,47531096],[89078499,18904913,25462145,60813308]]
-
+    #a = [[54581641,64080174,24346381,69107959],[86374198,61363882,68783324,79706116],[668150,92178815,89819108,94701471],[83920491,22724204,46281641,47531096],[89078499,18904913,25462145,60813308]]
+    a = [
+        [1,2,3],
+        [0,0,4],
+        [7,6,5]
+        ]
+        
     a1 = S.cutOffTree(a)
     
     print(a1)
