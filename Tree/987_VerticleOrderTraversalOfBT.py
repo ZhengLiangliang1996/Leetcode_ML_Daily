fd4c8645999d48c1963502ab024d585cad0cc287
ZhengLiangliang1996 <zhengliangliang1996@gmail.com>
2021-01-29 21:49:11 +0100

    2021 Jan Challenge Day2

--- Tree/987_VerticleOrderTraversalOfBT.py
+++ Tree/987_VerticleOrderTraversalOfBT.py
@@ -12,30 +12,29 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # 细节　两个坐标相同的时候　按照顺序进行输出
-        
-        # BFS 
-        res1 = [] 
+
+        # BFS
+        res1 = []
         if not root:
             return res
-        
+
         q = []
         q.append((root, 0, 0))
-        
+
         while q:
             node, x, y = q.pop()
-            # -y是因为后面sort的时候按照坐标排序
+            # +y是因为后面sort的时候按照坐标排序
             res1.append((x, y, node.val))
-            
+
             if node.left != None:
                 q.append((node.left, x - 1, y + 1))
             if node.right != None:
                 q.append((node.right, x + 1, y + 1))
-                
+
         res1.sort()
 
 
         res2 = [[res1[0][2]]]
-        # res2 = []
         for i in range(1, len(res1)):
             if res1[i][0] == res1[i - 1][0]:
                 res2[-1].append(res1[i][2])
@@ -44,4 +43,4 @@ class Solution(object):
 
 
         return res2
-        
+
