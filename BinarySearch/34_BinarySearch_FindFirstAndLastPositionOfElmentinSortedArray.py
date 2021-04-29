00e475bb7a68a90e991bc465b2758b3eecce1009
ZhengLiangliang1996 <zhengliangliang1996@gmail.com>
2021-04-29 20:29:58 +0200

    2021 update Apr Day 29

--- BinarySearch/34_BinarySearch_FindFirstAndLastPositionOfElmentinSortedArray.py
+++ BinarySearch/34_BinarySearch_FindFirstAndLastPositionOfElmentinSortedArray.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+
         l = 0
         r = len(nums) - 1
         res = []
-        # find first position 
-        # 
+        # find first position
+        #
         while l <= r:
             mid = l + ((r - l) >> 1)
             if(nums[mid] < target):
@@ -19,7 +19,7 @@ class Solution(object):
                 r = mid - 1
         ansLow = l
 
-        l = 0 
+        l = 0
         r = len(nums) - 1
 
         while l <= r:
@@ -31,6 +31,40 @@ class Solution(object):
         ansHigh = r
 
         return [ansLow, ansHigh] if ansLow <= ansHigh else [-1, -1]
+
+    def searchRange_2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if not nums or target not in nums:
+            return [-1, -1]
+
+        def bisec_left(nums, target):
+            l = 0
+            r = len(nums)
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] >= target:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
+
+        def bisec_right(nums, target):
+            l = 0
+            r = len(nums)
+            while l < r:
+                mid = l + (r - l) // 2
+                if nums[mid] <= target:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+
+
+        return [bisect_left(nums, target), bisect_right(nums, target)-1]
 def main():
 
     nums = [5, 7, 7, 8, 8, 10]
