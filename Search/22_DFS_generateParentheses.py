82979c87cddfcff4c7bb512ae2ed4ee33e825057
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2021-06-16 21:38:42 +0200

    16 June

--- Search/22_DFS_generateParentheses.py
+++ Search/22_DFS_generateParentheses.py
@@ -4,20 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        
         res = []
 
-        def dfs(left, right, string, res):
+        def backtrack(left, right, string, res):
             if left == 0 and right == 0:
                 res.append(string)
                 return 
             
             if left > 0:
-                dfs(left - 1, right, string + "(", res)
+                backtrack(left - 1, right, string + "(", res)
             if right > left:
-                dfs(left , right - 1, string + ")", res)
+                backtrack(left , right - 1, string + ")", res)
         
-        dfs(n, n, '',res)
+        backtrack(n, n, '',res)
         return res
 
 
