7a383a4fdf900dc80201c097467203d9007d677f
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2021-08-31 19:23:01 +0200

    31 Aug

---
+++ BinarySearch/153_FindMinimuminRotatedSortedArray.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2021 liangliang <liangliang@Liangliangs-MacBook-Air.local>
+#
+# Distributed under terms of the MIT license.
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return -1
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]
+
+    
