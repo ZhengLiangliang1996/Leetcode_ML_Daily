8cd14fbd4a9505748053947d0cbb69bf72762e97
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2022-04-25 23:50:05 +0200

    weekcontest49

---
+++ contest/weekcontest249/ConcatenationArray.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 liangliang <liangliang@Liangliangs-MacBook-Air.local>
+#
+# Distributed under terms of the MIT license.
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums*2
