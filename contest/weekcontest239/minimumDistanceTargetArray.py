9093eaa2e2b1aebf927257907e38fa4feba0e599
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2022-04-26 23:45:03 +0200

    weekcontest39

---
+++ contest/weekcontest239/minimumDistanceTargetArray.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 liangliang <liangliang@Liangliangs-MacBook-Air.local>
+#
+# Distributed under terms of the MIT license.
+class Solution(object):
+    def getMinDistance(self, nums, target, start):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        res = float('inf')
+        for i in range(len(nums)):
+            if nums[i] == target:
+                res = min(res, abs(start - i))
+
+        return res 
