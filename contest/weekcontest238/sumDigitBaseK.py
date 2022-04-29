0e10f269565ce0acb5d2f5b6ad54d0add58662a1
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2022-04-29 23:48:01 +0200

    weekcontest238

---
+++ contest/weekcontest238/sumDigitBaseK.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 liangliang <liangliang@Liangliangs-MacBook-Air.local>
+#
+# Distributed under terms of the MIT license.
+class Solution(object):
+    def sumBase(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def numberToBase(n, b):
+            if n == 0:
+                return [0]
+            digits = []
+            while n:
+                digits.append(int(n % b))
+                n //= b
+            return digits[::-1]
+        return sum(numberToBase(n, k))
