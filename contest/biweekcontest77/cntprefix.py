20b5088a77cc304d81529c80f538529d0d6d35fd
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2022-05-07 23:52:15 +0200

    biweekcontest77

---
+++ contest/biweekcontest77/cntprefix.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 liangliang <liangliang@Liangliangs-MacBook-Air.local>
+#
+# Distributed under terms of the MIT license.
+class Solution(object):
+    def countPrefixes(self, words, s):
+        """
+        :type words: List[str]
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(words)):
+            if words[i] == s[:len(words[i])]:
+                res += 1
+        
+        return res 
