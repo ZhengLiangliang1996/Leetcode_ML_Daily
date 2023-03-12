70101d2b5ea318858b999e6b83996ff48680b8eb
Zhengliangliang1996 <zhengliangliang1996@gmail.com>
2023-03-12 09:11:15 +0100

    2023_Day03

--- List/21_Mer2SortedList.py
+++ List/21_Mer2SortedList.py
@@ -3,26 +3,37 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode(-1) 
+        pre = res 
+        if not list1 and not list2:
+            return list1
         
-        head = ListNode(0)
-        move = head
-        if not l1 or not l2:
-            return l1 or l2
-        while l1 and l2:
-            if l1.val < l2.val:
-                move.next = l1
-                l1 = l1.next
+        if list1 and not list2: return list1
+        if not list1 and list2: return list2
+
+        while list1 and list2: 
+            if list1.val < list2.val:
+                res.next = list1
+                res = res.next 
+                list1 = list1.next
             else:
-                move.next = l2
-                l2 = l2.next
-            move = move.next
-        move.next = l1 or l2
-        return head.next
+                res.next = list2
+                res = res.next 
+                list2 = list2.next
+
+        if list2: 
+            res.next = list2 
+        
+        if list1:
+            res.next = list1
+        
+        return pre.next 
+
+        
